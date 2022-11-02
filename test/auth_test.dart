a04ce803eaad0bf5721a89787fa6b4a9dcbc70aa
mishapark <66741910+mishapark@users.noreply.github.com>
2022-11-01 21:53:20 -0400

    step-16

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -108,6 +108,7 @@ class MockAuthProvider implements AuthProvider {
     if (email == 'foo@bar.com') throw UserNotFoundAuthException();
     if (password == 'foobar') throw WrongPasswordAuthException();
     const user = AuthUser(
+      id: '1',
       isEmailVerified: false,
       email: 'foo@bar.com',
     );
@@ -129,6 +130,7 @@ class MockAuthProvider implements AuthProvider {
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
     const newUser = AuthUser(
+      id: '1',
       isEmailVerified: true,
       email: 'foo@bar.com',
     );
